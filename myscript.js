e199299d5a54e755b394609fe2dab053128aff43
loganpowers <77456953+loganpowers@users.noreply.github.com>
2021-04-26 11:37:00 -0500

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,1 +1,21 @@
+document.getElementById("addBtn").addEventListener("click", addText);
 
+function addText(){
+  var listItem = document.createElement("li");
+  listItem.InnerText = document.getElementById("myInput").value;
+  document.getElementById("myUl").appendChild(listItem);
+  
+  //delete button
+  var deleteButton = document.createElement("button");
+  deleteButton.className = "delete";
+  deleteButton.onclick=removeText;
+  deleteButton.innerText = "x";
+  listItem.appenedChild(deleteButton);
+}
+
+function removeText(){
+  var listItem=this.parentNode;
+  var ul=listItem.parentNode;
+  ul.removeChild(listItem);
+  
+}
